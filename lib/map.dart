fa20c0cc54709621f8d593f3486f72520f3f5b09
AnexAntony <anexantony278@gmail.com>
2024-04-11 12:12:36 +0530

    added Google Maps dependency

--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class VaayoMap extends StatefulWidget {
   const VaayoMap({super.key});
@@ -12,47 +11,9 @@ class VaayoMap extends StatefulWidget {
 class _VaayoMapState extends State<VaayoMap> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColorLight,
-          title: const Text("Google search places API"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: ListView(
-            children: [
-              const Text("To"),
-              TextField(
-                onChanged: (value) {
-                  debugPrint(value);
-                },
-              ),
-              Padding(padding: EdgeInsets.all(10)),
-              SizedBox(
-                  width: MediaQuery.of(context).size.width - 80,
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: FlutterMap(
-                      options: MapOptions(
-                        initialCenter: LatLng(9.853135, 76.947712),
-                        initialZoom: 17,
-                        backgroundColor: Colors.red,
-                      ),
-                      children: [
-                        TileLayer(
-                          urlTemplate:
-                              'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                          subdomains: [
-                            'a',
-                            'b',
-                            'c'
-                          ], // Subdomains for the tile server
-                        ),
-                      ]))
-            ],
-          ),
-        ),
-      ),
-    );
+    return const GoogleMap(
+        initialCameraPosition: CameraPosition(
+            target: const LatLng(37.42796133580664, -122.085749655962),
+            zoom: 13));
   }
 }
