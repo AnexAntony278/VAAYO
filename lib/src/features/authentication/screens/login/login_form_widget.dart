999f0f92711774918c26b1721d9b2c4c909e26cf
AnexAntony <anexantony278@gmail.com>
2024-04-27 00:01:39 +0530

    login validation

--- lib/src/features/authentication/screens/login/login_form_widget.dart
+++ lib/src/features/authentication/screens/login/login_form_widget.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vaayo/main.dart';
 import 'package:vaayo/src/features/authentication/screens/forget-password/forget_password_model_button_sheet.dart';
-import 'package:flutter/material.dart';
+
+final _formKey = GlobalKey<FormState>();
 
 class LoginForm extends StatefulWidget {
   const LoginForm({
@@ -23,18 +25,23 @@ class _LoginFormState extends State<LoginForm> {
     return Form(
         child: Container(
       padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
               controller: _emailTextController,
               decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.person_outline_outlined),
                   labelText: "Email",
                   border: OutlineInputBorder(),
-                  hintText: "Email")),
-          const SizedBox(height: 10.0),
-          TextFormField(
+                  hintText: "Email"),
+              validator: validateEmail,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            const SizedBox(height: 10.0),
+            TextFormField(
               controller: _passwordTextController,
               obscureText: true,
               decoration: InputDecoration(
@@ -44,50 +51,83 @@ class _LoginFormState extends State<LoginForm> {
                   hintText: "Passsword",
                   suffixIcon: IconButton(
                       onPressed: () {},
-                      icon: const Icon(Icons.remove_red_eye_sharp)))),
-          const SizedBox(height: 10.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                errorMessage,
-                style: const TextStyle(color: Colors.red),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                    onPressed: () {
-                      ForgetPasswordScreen.buildShowModelBottomSheet(context);
-                    },
-                    child: const Text("Forgot Password?")),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () async {
-                  try {
-                    final UserCredential userCredential =
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: _emailTextController.text,
-                            password: _passwordTextController.text);
-                    final prefs = await SharedPreferences.getInstance();
-                    prefs.setString('uid', userCredential.user?.uid ?? 'null');
-                    debugPrint("\nLoginPage: uid:${prefs.getString('uid')}");
-                    navKey.currentState?.pop();
-                    navKey.currentState?.pushNamed("Home");
-                  } on FirebaseAuthException catch (e) {
-                    setState(() {
-                      errorMessage = e.code;
-                    });
-                    //TODO- EXCEPTION HANDLING
-                  }
-                },
-                child: const Text(" LOGIN")),
-          ),
-        ],
+                      icon: const Icon(Icons.remove_red_eye_sharp))),
+              validator: validatePassword,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  errorMessage,
+                  style: const TextStyle(color: Colors.red),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {
+                        ForgetPasswordScreen.buildShowModelBottomSheet(context);
+                      },
+                      child: const Text("Forgot Password?")),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: userLogin, child: const Text(" LOGIN")),
+            ),
+          ],
+        ),
       ),
     ));
+  }
+
+  void userLogin() async {
+    try {
+      final UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _emailTextController.text,
+              password: _passwordTextController.text);
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('uid', userCredential.user?.uid ?? 'null');
+      debugPrint("\nLoginPage: uid:${prefs.getString('uid')}");
+      navKey.currentState?.pop();
+      navKey.currentState?.pushNamed("Home");
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        errorMessage = e.code;
+      });
+      //TODO- EXCEPTION HANDLING
+    }
+  }
+
+  String? validateEmail(String? email) {
+    RegExp emailRegex = RegExp(r'^[a-zA-Z0-9._-]{6,}@(gmail\.com)$');
+    final isEmailValid = emailRegex.hasMatch(email ?? '');
+    if (!isEmailValid) {
+      return "Please enter a valid email";
+    }
+    return null;
+  }
+
+  String? validatePassword(String? password) {
+    if (password == null || password.isEmpty) {
+      return 'Password is required';
+    }
+    if (password.length < 6) {
+      return 'Password must be at least 8 characters long';
+    }
+    if (!password.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter';
+    }
+    if (!password.contains(RegExp(r'[a-z]'))) {
+      return 'Password must contain at least one lowercase letter';
+    }
+    if (!password.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number';
+    }
+    return null; // Return null if password is valid
   }
 }
